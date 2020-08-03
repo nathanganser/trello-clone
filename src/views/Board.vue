3faf4b89c88a0e5776405319fdf0f9909a9aaca2
Nathan Ganser <me@nathanganser.com>
2020-08-03 09:10:47 +0200

    initial commit

---
+++ src/views/Board.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="board">
+
+  </div>
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+
+export default {
+  computed: mapState(['board'])
+}
+</script>
+
+<style lang="css">
+.task {
+  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
+}
+
+.column {
+  @apply bg-grey-light p-2 mr-4 text-left shadow rounded;
+  min-width: 350px;
+}
+
+.board {
+  @apply p-4 bg-teal-dark h-full overflow-auto;
+}
+
+.task-bg {
+  @apply pin absolute;
+  background: rgba(0,0,0,0.5);
+}
+</style>
