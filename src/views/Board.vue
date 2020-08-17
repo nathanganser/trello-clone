da7c5e99ae001d64416245fbc94489bf7a6c57de
Nathan Ganser <me@nathanganser.com>
2020-08-17 12:11:04 +0200

    added some stuff

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,14 +1,60 @@
 <template>
   <div class="board">
+    <div class="flex flex-row items-start">
+      <div
+        class="column"
+        v-for="column of board.columns"
+      >
 
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+
+        <div class="list-reset">
+          <div
+            class="task"
+            v-for="task of column.tasks"
+            @click="goToTask(task)"
+          >
+            <span class="w-full flex-no-shrink font-bold">{{ task.name }}</span>
+            <p
+              class="w-full flex-no-shrink mt-1 text-sm"
+              v-if="task.description"
+            >{{ task.description}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="task-bg"
+      v-if="isTaskOpen"
+      @click.self="close"
+    >
+      <router-view/>
+
+    </div>
   </div>
 </template>
 
 <script>
-  import { mapState } from 'vuex'
+import { mapState } from 'vuex'
 
 export default {
-  computed: mapState(['board'])
+  computed: {
+    ...mapState(['board']),
+    isTaskOpen () {
+      return this.$route.name === 'task'
+    }
+  },
+  methods: {
+    goToTask (task) {
+      this.$router.push({ name: 'task', params: {id: task.id }})
+    },
+    close () {
+      this.$router.push( { name: 'board'})
+    }
+  }
 }
 </script>
 
